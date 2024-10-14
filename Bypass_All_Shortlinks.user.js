afb008eb1407e24e34aad4dc5c9d8c2bdcea5917
p0008874 <p0008874@noreply.codeberg.org>
2024-10-14 09:34:44 +0000

    Add Rinku Domain

--- Bypass_All_Shortlinks.user.js
+++ Bypass_All_Shortlinks.user.js
@@ -4,7 +4,7 @@
 // @run-at     document-start
 // @author     Amm0ni4
 // @noframes
-// @version        92.8.22
+// @version        92.8.23
 // @grant          GM_setValue
 // @grant          GM_getValue
 // @grant          GM_addStyle
