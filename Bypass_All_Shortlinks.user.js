237f08521fd5c4477f77ad9bef3cf48935b85dda
Amm0ni4 <gi0a0pc2@anonaddy.me>
2025-05-16 13:46:18 +0200

    disable broken psa bypass site
    https://codeberg.org/Amm0ni4/bypass-all-shortlinks-debloated/issues/308

--- Bypass_All_Shortlinks.user.js
+++ Bypass_All_Shortlinks.user.js
@@ -4,7 +4,7 @@
 // @run-at     document-start
 // @author     Amm0ni4
 // @noframes
-// @version    96.1
+// @version    96.1.1
 // @grant      GM_setValue
 // @grant      GM_getValue
 // @grant      GM_addStyle
@@ -281,7 +281,6 @@
 // @match *://*.shrs.link/*
 // @match *://*.shareus.io/*
 // @match *://*.bloggerpemula.pythonanywhere.com/*
-// @grant      GM_deleteValue
 // @match        https://acortalink.me/*
 // @match         *://linkvertise.com/*
 // @match        *://bstlar.com/*
@@ -292,8 +291,6 @@
 // @match       *://www.ryuugames.com/?eroge=*
 // @match       *://anchoreth.com/r-adsh?t=i&v=*
 // @match        https://paster.so/*
-// @match        https://psa.wf/goto/*
-// @match        https://moviezapiya.fun/
 // @include       /^https:\/\/((bleleadersto|tonordersitye|daughablelea|mdlinkshub).com)\/s\?(?!.*f933e7ff).*$/
 // @include       /^(https?:\/\/)(loot-link.com|loot-links.com|lootlink.org|lootlinks.co|lootdest.(info|org|com)|links-loot.com|linksloot.net)\/s\?.*$/
 // @include     /(mundopolo.net|myfirstdollar.net|adsense.tupaste.top|acorta2.com|web.admoneyclick.net|acortaphd.live|onlypc.net|link.manudatos.com)/
@@ -3483,77 +3480,6 @@ function redirectWithMessage(url) {
             protectButtons();
         });
 
-    }
-})();
-//-------
-
-//---PSA Bypasser through moviezapiya.fun----
-(function() {
-    'use strict';
-    const currentURL = window.location.href;
-
-    // First site: psa.wf/goto/*
-    if (currentURL.match(/https:\/\/psa\.wf\/goto\/.*/)) {
-        // Immediately save the current URL without waiting
-        GM_setValue('savedLink', currentURL);
-        console.log('Saved URL:', currentURL);
-
-        // Redirect to the second site
-        window.location.href = 'https://moviezapiya.fun/';
-    }
-    // Second site: moviezapiya.fun
-    else if (currentURL.match(/https:\/\/moviezapiya\.fun\/.*/)) {
-        // Wait for the page to be fully loaded before proceeding
-        document.addEventListener('DOMContentLoaded', function() {
-            // Add additional delay to handle any Cloudflare or other protections
-            setTimeout(processMoviezApiyaSite, 1000);
-        });
-    }
-
-    function processMoviezApiyaSite() {
-        const savedLink = GM_getValue('savedLink', null);
-        console.log('Retrieved saved URL:', savedLink);
-
-        if (!savedLink) {
-            console.error('No saved link found.');
-            return;
-        }
-
-        // Check if the form exists
-        const inputField = document.querySelector('#userLink');
-        const bypassButton = document.querySelector('.container > button:nth-child(4)');
-
-        if (!inputField || !bypassButton) {
-            console.log('Required elements not found. Retrying in 1 second...');
-            setTimeout(processMoviezApiyaSite, 1000);
-            return;
-        }
-
-        // Fill the form with the saved link
-        inputField.value = savedLink;
-
-        // Clear the variable
-        GM_deleteValue('savedLink');
-
-        // Click the bypass button
-        console.log('Clicking bypass button...');
-        bypassButton.click();
-
-        // Start checking for the result link
-        checkForResultLink();
-    }
-
-    function checkForResultLink() {
-        const resultLink = document.querySelector('#resultLink');
-
-        if (resultLink && resultLink.href && resultLink.href.includes('get-to.link')) {
-            console.log('Final link found:', resultLink.href);
-            // Redirect to the final link
-            window.location.href = resultLink.href;
-        } else {
-            console.log('Final link not found yet. Checking again in 2 seconds...');
-            setTimeout(checkForResultLink, 2000); // Check again in 2 seconds
-        }
     }
 })();
 //-------
--- untouched_Bypass_All_Shortlinks_patched.user.js
+++ untouched_Bypass_All_Shortlinks_patched.user.js
@@ -37,7 +37,7 @@
 // @namespace  Violentmonkey Scripts
 // @run-at     document-start
 // @author     Bloggerpemula
-// @version    96.1
+// @version    96.1.1
 // @match      *://*/*
 // @grant      GM_setValue
 // @grant      GM_getValue
